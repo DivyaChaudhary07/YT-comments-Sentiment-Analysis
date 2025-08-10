d011381b7924482d2012ecdc6bbd752dc7a7ffc3
DivyaChaudhary07 <chaudharydivya713@gmail.com>
2025-08-10 21:31:09 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'Divya Chaudhary',
+    author_email= '',
+    packages= find_packages(),
+    install_requires = []
+
+)
